5d2e61183029d37a9ab3fd49a73d0cee62628861
LuminousDefex <defex84@gmail.com>
2020-12-14 16:29:27 +0200

    registration complete

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,9 @@
 const express = require("express")
 const router = express.Router()
+const bcrypt = require("bcryptjs")
+
+//User model 
+const User = require("../models/User")
 
 //desc:     Dashboard
 //route:    Get /dashboard
@@ -42,7 +46,40 @@ router.post("/register", (req, res) => {
             password2
         })
     } else {
-        res.send("pass");
+        // Validation passed
+        User.findOne({ email: email })
+            .then(user => {
+                if (user) {
+                    errors.push({ msg: "Email is already in use" })
+                    res.render("register", {
+                        errors,
+                        name,
+                        email,
+                        password,
+                        password2
+                    })
+                } else {
+                    const newUser = new User({
+                        name,
+                        email,
+                        password
+                    })
+
+                    // Hash Password
+                    bcrypt.genSalt(10, (err, salt) =>
+                        bcrypt.hash(newUser.password, salt, (err, hash) => {
+                            if (err) throw err;
+                            // set password to hashed
+                            newUser.password = hash;
+                            //save user
+                            newUser.save()
+                                .then(user => {
+                                    res.redirect("/");
+                                })
+                                .catch(err => console.log(err));
+                        }))
+                }
+            })
     }
 })
 
