c8be8d94f70e419ec135cf289f28cce81ad220cb
LuminousDefex <defex84@gmail.com>
2020-12-16 17:06:33 +0200

    Update download.js

--- public/scripts/download.js
+++ public/scripts/download.js
@@ -2,7 +2,6 @@
 var container = document.querySelector('textarea');
 var anchor = document.querySelector('#dBtn');
 anchor.onclick = function () {
-    console.log(container.value)
     anchor.href = 'data:text/plain;charset=utf-8,' + encodeURIComponent(container.value);
     anchor.download = 'export.txt';
 };
