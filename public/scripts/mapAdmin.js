533e8bcf5745c8cdb444bbf5f14c69e285a057d8
LuminousDefex <defex84@gmail.com>
2021-01-10 16:48:57 +0200

    added admin map

--- public/scripts/mapAdmin.js
+++ public/scripts/mapAdmin.js
@@ -8,6 +8,37 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     accessToken: "pk.eyJ1IjoiZGVmZXgiLCJhIjoiY2tqMTg1eXZhMGlhdTJzbGdjMDVybmthZyJ9.Z6HlHQllZ1tohkK3pDAsLA"
 }).addTo(map);
 
+let userLatLong = JSON.parse(document.getElementById("userLatLong").value);
+let userIpMarker = Object.values(userLatLong);
+
+let ipLatLong = JSON.parse(document.getElementById("ipLatLong").value);
+let ipMarker = ipLatLong;
+let markers = [];
+let markersInit = [];
+let weights = [];
+for (entry of ipMarker) {
+    let weight = entry.intensity * 10;
+    delete entry["intensity"];
+    markersInit.push(Object.values(entry))
+    let tempArr = [];
+    tempArr.push(userIpMarker, Object.values(entry));
+    markers.push(tempArr)
+    weights.push(weight);
+}
+
 $(document).ready(function () {
+    L.marker(userIpMarker).addTo(map);
+    for (let marker of markersInit) {
+        L.marker(marker).addTo(map);
+    }
+
+
+    for (let i = 0; i < markers.length; i++) {
+        L.polyline(markers[i], {
+            noClip: true,
+            color: "blue",
+            weight: weights[i],
+        }).addTo(map);
+    }
 });
 
